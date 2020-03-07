9b9a58e2409e4ecf441885cc9ddf0f1d181e8c0b
srinivas2000 <61343158+srinivas2000@users.noreply.github.com>
2020-03-07 12:22:09 +0530

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+int a,b;
+printf("Enter the value of a and b ");
+scanf("%d %d",&a, &b);
+printf(" The relation between the two numbers are \n");
+printf(" TRUE=1 FALSE=0 \n");
+printf("a > b: %d \n",a>b);
+printf("a < b: %d \n",a<b);
+printf("a >= b: %d \n",a>=b);
+printf("a <= b: %d \n",a<=b);
+printf("a == b: %d \n", a==b);
+printf("a != b: %d \n", a!=b);
+}
---
+++ salary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{ 
+ float BS,HA,DA;
+ printf("Enter the basic salary");
+ scanf("%f",&BS);
+ HA=(0.2)*BS;
+ DA=(0.4)*BS;
+ printf("The HA is %f \n",HA);
+ printf("The DA is %f \n",DA);
+}
+  
